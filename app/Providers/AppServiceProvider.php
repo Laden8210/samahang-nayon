34d6a16d252d06c611bb23ccea83e375e95cd61e
Laden8210 <domingo.laden@gmail.com>
2024-08-27 12:46:55 +0800

    haha

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
+    protected $namespace = 'App\Http\Controllers';
     public function register(): void
     {
         //
