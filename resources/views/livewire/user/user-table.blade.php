3bc6225939a42e916d1c1d0a5c6b6e65e1ed63f1
Laden8210 <domingo.laden@gmail.com>
2024-10-22 02:38:10 +0800

    update

--- resources/views/livewire/user/user-table.blade.php
+++ resources/views/livewire/user/user-table.blade.php
@@ -130,7 +130,7 @@
                 </p>
             </div>
             <div class="flex items-center">
-                {{$users->links('vendor.livewire.bootstrap')}}
+                {{$users->links()}}
             </div>
         </div>
 
