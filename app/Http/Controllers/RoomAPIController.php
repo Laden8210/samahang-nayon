e1b4f7347037402ba03ba58eab55434c63985a44
Laden8210 <domingo.laden@gmail.com>
2024-10-23 14:35:41 +0800

    update

--- app/Http/Controllers/RoomAPIController.php
+++ app/Http/Controllers/RoomAPIController.php
@@ -66,7 +66,7 @@ class RoomAPIController extends Controller
             return !$roomNumber->isBooked; // Keep only rooms that are not booked
         })->map(function ($roomNumber) {
             return [
-                'room_number_id' => $roomNumber->room_number_id,
+                'room_number_id' => $roomNumber->room_number_id, // Correctly enclosed
                 'room_number' => $roomNumber->room_number,
                 'RoomId' => $roomNumber->room->RoomId,
                 'deleted_at' => $roomNumber->deleted_at,
