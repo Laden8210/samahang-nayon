5381dc0d5bf45361ef7768c45041ee535c7431d3
Laden8210 <domingo.laden@gmail.com>
2024-10-30 13:13:10 +0800

    update

--- resources/views/download-app.blade.php
+++ resources/views/download-app.blade.php
@@ -33,7 +33,8 @@
             breathtaking views, top-notch facilities, and exceptional service, we ensure that your stay is comfortable
             and memorable.
         </p>
-        <img src="https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/251812038_403610324789782_3574069820819899217_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHLdnur_m8EX3IfXhxJGNA9TVhfm02YQtxNWF-bTZhC3NY8-doMc-6VgEO3Xd805Xcu4YnozWfCtwsEy-CgG9h-&_nc_ohc=sLM6LkuCTscQ7kNvgEV0fQv&_nc_zt=23&_nc_ht=scontent.fdvo2-2.fna&_nc_gid=A1V880UlKgSdkXPrC0kYvGK&oh=00_AYAM0P1TzDXJl0BN-dbUOziL5thwuMuYA6p9qlR0lABBtA&oe=672784FC" alt="Beautiful view of Samahang Nayon hotel" class="rounded-lg shadow-lg mx-auto">
+        <img src="https://scontent.fdvo2-2.fna.fbcdn.net/v/t39.30808-6/251812038_403610324789782_3574069820819899217_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHLdnur_m8EX3IfXhxJGNA9TVhfm02YQtxNWF-bTZhC3NY8-doMc-6VgEO3Xd805Xcu4YnozWfCtwsEy-CgG9h-&_nc_ohc=sLM6LkuCTscQ7kNvgEV0fQv&_nc_zt=23&_nc_ht=scontent.fdvo2-2.fna&_nc_gid=A1V880UlKgSdkXPrC0kYvGK&oh=00_AYAM0P1TzDXJl0BN-dbUOziL5thwuMuYA6p9qlR0lABBtA&oe=672784FC"
+            alt="Beautiful view of Samahang Nayon hotel" class="rounded-lg shadow-lg mx-auto">
     </section>
 
     <!-- Features Section -->
@@ -60,14 +61,16 @@
         </section>
 
         <!-- Download Section -->
-        <section id="download" class="text-center bg-gradient-to-r from-green-500 to-green-600 p-10 text-white rounded-lg shadow-lg">
+        <section id="download"
+            class="text-center bg-gradient-to-r from-green-500 to-green-600 p-10 text-white rounded-lg shadow-lg">
             <h2 class="text-3xl font-bold mb-6">Book Your Stay Today</h2>
             <p class="mb-6">Available on both Android platforms. Start your journey with us now!</p>
             <div class="flex justify-center gap-4">
-
-                <a href="#" class="bg-white text-gray-800 font-semibold py-3 px-6 rounded-full shadow-lg hover:bg-gray-100 transition duration-300">
+                <a href="https://mega.nz/folder/j08miK4Q#j7hMP4YuYuw_p4_Ikv9Crw" target="_blank"
+                    class="bg-white text-gray-800 font-semibold py-3 px-6 rounded-full shadow-lg hover:bg-gray-100 transition duration-300">
                     <i class="fab fa-google-play mr-2"></i>Download for Android
                 </a>
+
             </div>
         </section>
     </main>
@@ -76,4 +79,5 @@
     @livewireScripts
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
